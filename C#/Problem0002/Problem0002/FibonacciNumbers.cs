6ea2144e57db65314e8e7b963ddb73b656cf9ee1
lehtomin <minna.j.lehtomaki@gmail.com>
2019-02-15 18:49:38 +0200

    Problem 2: Moved the generation of Fibonacci series to a new class

---
+++ C#/Problem0002/Problem0002/FibonacciNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Problem0002
+{
+    public class FibonacciNumbers
+    {
+        public List<int> GenerateFibonacciSeries(int limitValueTo)
+        {
+            var fibonacciSeries = new List<int> { 1, 1 };
+            for (var i = 1; fibonacciSeries[i] < limitValueTo; i++)
+            {
+                fibonacciSeries.Add(fibonacciSeries[i] + fibonacciSeries[i - 1]);
+            }
+
+            return fibonacciSeries;
+        }
+    }
+}
--- C#/Problem0002/Problem0002/Program.cs
+++ C#/Problem0002/Problem0002/Program.cs
@@ -7,23 +7,13 @@ namespace Problem0002
     {
         public static void Main()
         {
+            var fibonacciNumbers = new FibonacciNumbers();
             // If value is bigger than 2147483647 (MaxValue), it will overflow the int
-            List<int> fibonacciSeries = GenerateFibonacciSeries(4000000);
+            List<int> fibonacciSeries = fibonacciNumbers.GenerateFibonacciSeries(4000000);
             var result = CalculateSumOfEvenNumbers(fibonacciSeries);
 
             Console.WriteLine(result);
             Console.ReadKey();
-        }
-
-        private static List<int> GenerateFibonacciSeries(int limitValueTo)
-        {
-            var fibonacciSeries = new List<int> { 1, 1 };
-            for (var i = 1; fibonacciSeries[i] < limitValueTo; i++)
-            {
-                fibonacciSeries.Add(fibonacciSeries[i] + fibonacciSeries[i - 1]);
-            }
-
-            return fibonacciSeries;
         }
 
         private static int CalculateSumOfEvenNumbers(List<int> values)
